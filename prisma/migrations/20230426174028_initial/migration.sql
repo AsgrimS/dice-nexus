9097170a5d724e81b0679a267f57c58c0032acbb
AsgrimS <jan.semik@gmail.com>
2023-04-26 19:40:38 +0200

    init migration

--- prisma/migrations/20230426174028_initial/migration.sql
+++ prisma/migrations/20230426174028_initial/migration.sql
@@ -10,9 +10,11 @@ CREATE TABLE `Game` (
 -- CreateTable
 CREATE TABLE `Player` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `Player_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
